e46552714c29ec1e4be06137b2ab085e04e7b507
Khushi Agarwal <khushiagarwalbly@gmail.com>
2026-03-30 22:37:35 +0530

    Fix db connection to use public Railway host:

--- db.py
+++ db.py
@@ -5,10 +5,11 @@ load_dotenv()
 
 def get_connection():
     conn = mysql.connector.connect(
-        host=os.getenv('MYSQLHOST', 'mysql.railway.internal'),
-        port=int(os.getenv('MYSQLPORT', 3306)),
+        host=os.getenv('MYSQLHOST', 'hopper.proxy.rlwy.net'),
+        port=int(os.getenv('MYSQLPORT', 15308)),
         user=os.getenv('MYSQLUSER', 'root'),
-        password=os.getenv('MYSQLPASSWORD', 'MbZigxtxrUeYarCWbQERDcNLbBujZxuP'),
-        database=os.getenv('MYSQLDATABASE', 'railway')
+        password=os.getenv('MYSQLPASSWORD', 'OuuDqvoZFqWKlIvVqFaErTdTJPcNQjRo'),
+        database=os.getenv('MYSQLDATABASE', 'railway'),
+        connection_timeout=30
     )
     return conn
