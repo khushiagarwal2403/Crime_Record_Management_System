6139760551c2d5184a9176906cf81f64116f7354
Khushi Agarwal <khushiagarwalbly@gmail.com>
2026-03-30 13:11:37 +0530

    Update db.py for Railway MySQL variables

--- db.py
+++ db.py
@@ -1,10 +1,14 @@
 import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def get_connection():
     conn = mysql.connector.connect(
-        host='127.0.0.1',
-        user='root',
-        password='',
-        database='crms'
+        host=os.getenv('MYSQLHOST', 'mysql.railway.internal'),
+        port=int(os.getenv('MYSQLPORT', 3306)),
+        user=os.getenv('MYSQLUSER', 'root'),
+        password=os.getenv('MYSQLPASSWORD', 'MbZigxtxrUeYarCWbQERDcNLbBujZxuP'),
+        database=os.getenv('MYSQLDATABASE', 'railway')
     )
     return conn
